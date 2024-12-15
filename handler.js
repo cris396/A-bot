cec5752c8ff43c80b155fbb1b6a05f05b91eb824
cris396 <84878980+cris396@users.noreply.github.com>
2024-12-15 15:02:23 -0500

    Add files via upload

--- handler.js
+++ handler.js
@@ -480,7 +480,7 @@ console.error(e)
 
 global.dfail = (type, m, conn) => {
 const msg = {
-rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> ianalejandrook15x', 
+rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> Emma', 
 owner: '「👑」 *Esta función solo puede ser usada por mi desarrollador.', 
 mods: '「🤴🏻」 *Esta función solo puede ser usada por mis desarrolladores.*', 
 premium: '「🍧」 *Esta función solo es para usuarios Premium.', 
@@ -488,7 +488,7 @@ group: '「☁」 *Esta funcion solo puede ser ejecutada en grupos.*',
 private: '「🍭」 *Esta función solo puede ser usada en chat privado.*', 
 admin: '「👑」 *Este comando solo puede ser usado por admins.*', 
 botAdmin: '「🍁」 *Para usar esta función debo ser admin.*', 
-unreg: '「🌸」 *No te encuentras registrado, registrese para usar esta función*\n\n*/reg nombre.edad*\n\n*Ejemplo* : */reg ian.14*',
+unreg: '「🌸」 *No te encuentras registrado, registrese para usar esta función*\n\n*/reg nombre.edad*\n\n*Ejemplo* : */reg Emma.14*',
 restrict: '「💫」 *Esta característica esta desactivada.*'
 }[type];
 if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
